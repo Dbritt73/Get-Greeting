a0ec10a900f6c4d312e6a2625fdf5321245adb8a
Darrin Britton <brittod@wwu.edu>
2018-07-27 08:30:05 -0700

    initial commit

---
+++ GetGreeting.cs
@@ -0,0 +1,50 @@
+﻿using System.Management.Automation;
+
+namespace HelloWorld
+{
+
+    [Cmdlet(VerbsCommon.Get, "Greeting")]
+    public class Greeting : PSCmdlet
+    {
+        private string[] NameCollection;
+
+        [Parameter( Mandatory = true,
+                    ValueFromPipeline = true,
+                    ValueFromPipelineByPropertyName = true,
+                    Position = 0,
+                    HelpMessage = "Name of individual to greet")]
+        [Alias("Person, Name")]
+        public string[] Name {
+
+            get { return NameCollection; }
+            set { NameCollection = value; }
+
+        }
+
+        protected override void BeginProcessing() {
+
+            base.BeginProcessing();
+
+        }
+
+        protected override void ProcessRecord() {
+
+            foreach (string Name in NameCollection) {
+
+                WriteVerbose("Creating greeting for " + Name);
+                string greeting = "Hello, " + Name;
+                WriteObject(greeting);
+
+            }
+
+        }
+
+        protected override void EndProcessing() {
+
+            base.EndProcessing();
+
+        }
+
+    }
+
+}
